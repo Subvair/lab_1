0cb06eebc484e5ef397e3431aa150171546e43c3
subvair <vadimogienko42k@gmail.com>
2024-09-14 19:57:42 +0700

    код работает правильно, но не оформлен

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@ using namespace std;
 
 int main() {
     double a = 16.74;
+    
     double r3 = a * sqrt(3)/3;
-    double r5 = a * sqrt((1/2)+(sqrt(5)/10));
+    double r5 = a * sqrt(0.5 + sqrt(5)/10);
     double r8 = a * sqrt(1 + (sqrt(2)/2));
     double r10 = (a/2) * (1 + sqrt(5));
+    double rn = a / (2*sin(3.14/5));
 
     cout << r3 << endl;
     cout << r5 << endl;
     cout << r8 << endl;
     cout << r10 << endl;
+    cout << rn << endl;
 
 }
